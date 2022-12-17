6fbb4170c5ac6a8c19ef301263a51c26eb55e361
SerenityJane <serena.danger@gmail.com>
2022-12-17 23:20:11 +0900

    Second commit with all TO-DO's completed, solved serialVersionUID error, basic testing completed. To be completed: more testing and adding notes for the code added

--- BIT504_Assign2_PartAStudent/Board.java
+++ BIT504_Assign2_PartAStudent/Board.java
@@ -27,20 +27,17 @@ public class Board {
 	 /** Return true if it is a draw (i.e., no more EMPTY cells) */ 
 	public boolean isDraw() {
 		 
-		// TODO: Check whether the game has ended in a draw. 
+		// DONE: Check whether the game has ended in a draw. 
 		// Hint: Use a nested loop (see the constructor for an example). Check whether any of the cells content in the board grid are Player.Empty. If they are, it is not a draw.
 		// Hint: Return false if it is not a draw, return true if there are no empty positions left
-		for (int row = 0; row < GameMain.ROWS;) {
+		for (int row = 0; row < GameMain.ROWS; ++row) {
 			for (int col = 0; col < GameMain.COLS; ++col) {
-				if(Player.Empty != null) {
+				if(cells[row][col].content == Player.Empty ) {
 					return false;
-				}
+				}	
 			}
-		} 
-		return true;
-		
-
-		
+		}
+		return true; 				
 	}
 	
 	/** Return true if the current player "thePlayer" has won after making their move  */
